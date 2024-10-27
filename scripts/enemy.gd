289702f26d41c0a3335ab15679ea5a73b8655e29
GomenBoi <dasilvaroberto385@yahoo.com>
2024-10-27 10:15:51 +0000

    added scripts

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,8 +1,25 @@
-extends StaticBody2D
+extends Area2D
+
+@onready var projectile_timer = $Timer
+@onready var projectile_scene = preload("res://scenes/fire_projectile.tscn")
+@onready var root : Node2D = get_tree().get_root().get_child(1)
+@onready var Player : CharacterBody2D = root.get_node("CharacterBody2D")
 
 func _ready() -> void:
+	# Plays idle animation and connects timeout function for spawning projectiles
 	$AnimatedSprite2D.play("idle")
+	projectile_timer.connect("timeout", on_timeout)
+	projectile_timer.start()
 	
-func _physics_process(delta: float) -> void:
-	pass
+func on_timeout() -> void:
+	var instance : FireProjectile = projectile_scene.instantiate()
+	instance.global_position = global_position
+	var dir = (Player.global_position - global_position).normalized()
+	instance.global_rotation = dir.angle() + PI / 2.0
+	instance.z_index = Player.z_index + 1
+	instance.initial_direction = dir
+	
+	root.call_deferred("add_child", instance)
+	
+	
 	
